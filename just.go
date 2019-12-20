a0b8a785530ebd63eb840facb1823033087092a9
zyguan <zhongyangguan@gmail.com>
2019-12-21 02:04:55 +0800

    add support for testing (#4)

--- just.go
+++ just.go
@@ -9,35 +9,6 @@ import (
 
 type Catchable interface {
 	Why() error
-	Error() string
-}
-
-func wrap(err error, optMsgs ...string) Catchable {
-	if err == nil {
-		return nil
-	}
-	msg := strings.Join(optMsgs, ": ")
-	tracedErr := trace(err)
-	if tracedErr == nil {
-		tracedErr = err
-	}
-	return &caught{tracedErr, msg}
-}
-
-func AsCatchable(a interface{}, optMsgs ...string) Catchable {
-	switch e := a.(type) {
-	case Catchable:
-		if len(optMsgs) == 0 {
-			return e
-		}
-		return &caught{e.Why(), strings.Join(optMsgs, ": ")}
-	case error:
-		return wrap(e, optMsgs...)
-	case string:
-		return wrap(errors.New(e), optMsgs...)
-	default:
-		return wrap(fmt.Errorf("%v", a), optMsgs...)
-	}
 }
 
 type caught struct {
@@ -45,7 +16,12 @@ type caught struct {
 	msg string
 }
 
-func (c *caught) Why() error { return c.err }
+func (c *caught) Why() error {
+	if len(c.msg) > 0 {
+		return c
+	}
+	return c.err
+}
 
 func (c *caught) Cause() error { return c.err }
 
@@ -73,26 +49,25 @@ func (c *caught) Format(s fmt.State, verb rune) {
 }
 
 func Return(ptr *error) {
-	HandleRecovered(ptr, id, recover())
+	HandleRecovered(ptr, func(c Catchable) error { return c.Why() }, recover())
 }
 
-func HandleAndReturn(handle func(error) error) func(*error) {
+func HandleAndReturn(handle func(Catchable) error) func(*error) {
 	return func(ptr *error) { HandleRecovered(ptr, handle, recover()) }
 }
 
 func AnnotateAndReturn(msg string) func(*error) {
-	annotate := func(err error) error { return wrap(err, msg) }
-	return HandleAndReturn(annotate)
+	return HandleAndReturn(func(c Catchable) error { return &caught{c.Why(), msg} })
 }
 
-func Catch(handle func(err error)) {
-	HandleRecovered(nil, func(err error) error {
+func Catch(handle func(c Catchable)) {
+	HandleRecovered(nil, func(err Catchable) error {
 		handle(err)
 		return nil
 	}, recover())
 }
 
-func HandleRecovered(ptr *error, handle func(error) error, recovered interface{}) {
+func HandleRecovered(ptr *error, handle func(Catchable) error, recovered interface{}) {
 	if recovered == nil {
 		return
 	} else if x, ok := recovered.(Catchable); ok {
@@ -119,22 +94,6 @@ func (xs Values) Nth(i int) interface{} {
 	return xs[i]
 }
 
-func Try(xs ...interface{}) Values {
-	if err := ExtractError(xs...); err != nil {
-		panic(wrap(err))
-	}
-	return xs
-}
-
-func TryTo(msg string) func(...interface{}) Values {
-	return func(xs ...interface{}) Values {
-		if err := ExtractError(xs...); err != nil {
-			panic(wrap(err, msg))
-		}
-		return xs
-	}
-}
-
 func ExtractError(xs ...interface{}) error {
 	if len(xs) > 0 {
 		if e, ok := xs[len(xs)-1].(error); ok {
@@ -144,13 +103,69 @@ func ExtractError(xs ...interface{}) error {
 	return nil
 }
 
-func Throw(a interface{}) { panic(AsCatchable(a)) }
+func AsCatchable(a interface{}, optMsgs ...string) Catchable {
+	return trace.AsCatchable(a, optMsgs...)
+}
 
-func Throwf(format string, args ...interface{}) { Throw(fmt.Errorf(format, args...)) }
+func Try(xs ...interface{}) Values { return trace.Try(xs...) }
+
+func TryTo(msg string) func(...interface{}) Values { return trace.TryTo(msg) }
+
+func Throw(a interface{}) { trace.Throw(a) }
+
+func Throwf(format string, args ...interface{}) { trace.Throwf(format, args...) }
 
 type TraceFn func(error) error
 
-var trace = id
+func (f TraceFn) wrap(err error, optMsgs ...string) Catchable {
+	if err == nil {
+		return nil
+	}
+	msg := strings.Join(optMsgs, ": ")
+	tracedErr := f(err)
+	if tracedErr == nil {
+		tracedErr = err
+	}
+	return &caught{tracedErr, msg}
+}
+
+func (f TraceFn) AsCatchable(a interface{}, optMsgs ...string) Catchable {
+	switch e := a.(type) {
+	case Catchable:
+		if len(optMsgs) == 0 {
+			return e
+		}
+		return &caught{e.Why(), strings.Join(optMsgs, ": ")}
+	case error:
+		return f.wrap(e, optMsgs...)
+	case string:
+		return f.wrap(errors.New(e), optMsgs...)
+	default:
+		return f.wrap(fmt.Errorf("%v", a), optMsgs...)
+	}
+}
+
+func (f TraceFn) Try(xs ...interface{}) Values {
+	if err := ExtractError(xs...); err != nil {
+		panic(f.wrap(err))
+	}
+	return xs
+}
+
+func (f TraceFn) TryTo(msg string) func(...interface{}) Values {
+	return func(xs ...interface{}) Values {
+		if err := ExtractError(xs...); err != nil {
+			panic(f.wrap(err, msg))
+		}
+		return xs
+	}
+}
+
+func (f TraceFn) Throw(a interface{}) { panic(f.AsCatchable(a)) }
+
+func (f TraceFn) Throwf(format string, args ...interface{}) { f.Throw(fmt.Errorf(format, args...)) }
+
+var trace TraceFn = id
 
 func id(err error) error { return err }
 
@@ -159,4 +174,28 @@ func SetTraceFn(f func(error) error) {
 		f = id
 	}
 	trace = f
+}
+
+type TestingT interface {
+	Errorf(format string, args ...interface{})
+	FailNow()
+}
+
+type ErrorAssertion func(err error, msgAndArgs ...interface{})
+
+func Assert(t TestingT, optErrAssertions ...ErrorAssertion) TraceFn {
+	if len(optErrAssertions) == 0 {
+		optErrAssertions = append(optErrAssertions, func(err error, msgAndArgs ...interface{}) {
+			if err != nil {
+				t.Errorf("%+v", err)
+				t.FailNow()
+			}
+		})
+	}
+	return func(e error) error {
+		for _, assert := range optErrAssertions {
+			assert(e, e.Error())
+		}
+		return nil
+	}
 }
